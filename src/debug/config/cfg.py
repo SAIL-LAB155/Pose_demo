b7055f0410bf577b69991688de0c614c40a1a1c9
SAIL-LAB155 <saillab155@gmail.com>
2020-06-29 09:18:12 +0800

    rm debug

--- src/debug/config/cfg.py
+++
@@ -1,6 +0,0 @@
-yolo_cfg = "../../config/yolo_cfg/yolov3.cfg"
-yolo_weight = '../../weights/yolo/yolov3.weights'
-video_path = "video/origin/class.mp4"
-
-pose_weight = "../../weights/sppe/duc_se.pth"
-pose_cfg = None
--- src/debug/config/cfg_only_detections.py
+++
@@ -1,9 +0,0 @@
-gray_yolo_cfg = "model/gray/prune_0.93_keep_0.1.cfg"
-gray_yolo_weights = "model/gray/best.weights"
-black_yolo_cfg = "model/black/yolov3-spp-1cls.cfg"
-black_yolo_weights = "model/black/best_converted.weights"
-rgb_yolo_cfg = ""
-rgb_yolo_weights = ""
-
-video_path = "video/region/19.mp4"
-water_top = 40
