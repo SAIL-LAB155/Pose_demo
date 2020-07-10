10f9dc89a415ea0838c32899ab78964ce3bd1aa5
cheungbh <534660436@qq.com>
2020-07-10 20:06:51 +0800

    add CNN

---
+++ src/debug/config/cfg_with_CNN.py
@@ -0,0 +1,11 @@
+CNN_weight = "model/CNN/yoga_1104.pth"
+CNN_backbone = "shufflenet"
+CNN_class = ["boat", "boat", "chair", "chair", "tree", "tree", "triangle", "triangle"]
+
+yolo_cfg = "../../config/yolo_cfg/yolov3.cfg"
+yolo_weight = "../../weights/yolo/yolov3.weights"
+
+pose_weight = "../../weights/sppe/duc_se.pth"
+pose_cfg = None
+
+video_path = "video/withCNN/00_Trim.mp4"
